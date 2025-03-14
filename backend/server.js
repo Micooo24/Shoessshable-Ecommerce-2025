c4f82fcae440e393b722ad3497ef1602b35bd1ff
Micooo24 <mico.rabino24@gmail.com>
2025-03-14 22:49:29 +0800

    install neccessary components

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
 const mongoose = require('mongoose');
+const cloudinary = require('cloudinary').v2;
 require('dotenv').config();
 
 // App
